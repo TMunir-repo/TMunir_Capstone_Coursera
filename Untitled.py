df68e72cf94a51505365bdaea040ea222fc25818
Muhammad Talha Munir/COM/ISB <talha.munir@jazz.com.pk>
2020-02-25 23:19:04 +0500

    first-commit

---
+++ Untitled.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# This notebook is used to work on a capstone project in IBM-Data Science course
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[2]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
